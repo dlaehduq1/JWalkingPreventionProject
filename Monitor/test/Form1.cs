9a87f4f3b94bfb9c1427c60170c2fdfa70c8a81a
dlaehduq1 <dlaehduq2@naver.com>
2016-07-26 21:11:19 +0900

    모니터 폴더 초기화

---
+++ Monitor/test/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+            button6.BackColor = System.Drawing.Color.SkyBlue;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //카메라
+        {
+
+            button5.BackColor = System.Drawing.Color.Azure;
+            button6.BackColor = System.Drawing.Color.SkyBlue;
+            panel5.Visible = false;
+           
+            panel6.Visible = true;
+            panel7.Visible = true;
+            panel8.Visible = true;
+            panel9.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e) // 위치
+        {
+
+            button5.BackColor = System.Drawing.Color.SkyBlue;
+            button6.BackColor = System.Drawing.Color.Azure;
+            panel5.Visible = true;
+         
+            panel6.Visible = false;
+            panel7.Visible = false;
+            panel8.Visible = false;
+            panel9.Visible = false;
+        }
+
+
+    }
+}
